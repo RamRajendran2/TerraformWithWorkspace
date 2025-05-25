282c88af6c4b37bbdf71fb53fa0dd11dbabe0509
RamRajendran2 <55951105+RamRajendran2@users.noreply.github.com>
2025-05-25 16:04:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,28 @@ variable "resource_group_location" {
   default     = "South India"
   description = "Location of the resource group."
 }
+
+
+variable "azure_client_id" {
+  description = "Azure AD Client ID for service principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_client_secret" {
+  description = "Azure AD Client Secret for service principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  description = "Azure AD Tenant ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  sensitive   = true
+}
